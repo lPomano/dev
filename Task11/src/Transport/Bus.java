61941ff399774eb043d0fff0e415b90797fa51c3
Pomano <Pomano>
2016-10-26 23:38:34 +0300

    Task11

---
+++ Task11/src/Transport/Bus.java
@@ -0,0 +1,29 @@
+package Transport;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+import static FileWork.FileReader.distance;
+
+/**
+ * Class Bus responsibles for the properties of the bus
+ */
+public class Bus {
+    final double averageSpeed = 60;
+    final double fuelConsumption = 25;
+    final double fuelCost = 0.58;
+    final double passengersQuantity = 20;
+
+    /**
+     * Method busTravel displays travel's time and cost
+     */
+    public void busTravel() {
+        DecimalFormatSymbols s = new DecimalFormatSymbols();
+        s.setDecimalSeparator('.');
+        DecimalFormat f = new DecimalFormat("#,##0.00", s);
+        double busTime = distance / averageSpeed;
+        System.out.println("Travel through our checkpoint by bus takes: " + f.format(busTime * 60) + " minutes");
+        double travelCost = ((distance / fuelConsumption) * fuelCost) / passengersQuantity;
+        System.out.println("And it will cost: " + f.format(travelCost) + "$" + " for every person" + "\n");
+    }
+}
---
+++ Task11/src/Transport/OnFoot.java
@@ -0,0 +1,25 @@
+package Transport;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+import static FileWork.FileReader.distance;
+
+/**
+ * Class OnFoot responsibles for the properties of the human
+ */
+public class OnFoot {
+    final double averageSpeed = 3;
+
+    /**
+     * Method onFootTravel displays travel's time and cost
+     */
+    public void onFootTravel() {
+        DecimalFormatSymbols s = new DecimalFormatSymbols();
+        s.setDecimalSeparator('.');
+        DecimalFormat f = new DecimalFormat("#,##0.00", s);
+        double onFootTime = distance / averageSpeed;
+        System.out.println("Travel through our checkpoint by foots takes: " + f.format(onFootTime*60) + " minutes");
+        System.out.println("And it will be free");
+    }
+}
